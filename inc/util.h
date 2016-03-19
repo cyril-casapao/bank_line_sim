80aafc36f81ea90c65aed8b4f44aa187022b0336
cyril-casapao <casapaoc@gmail.com>
2016-03-19 15:57:08 -0400

    Correctly implement file reading.

---
+++ inc/util.h
@@ -0,0 +1,19 @@
+/**
+* util.h
+*
+* @author Cyril Casapao
+*/
+#ifndef _UTIL_H_
+#define _UTIL_H_
+
+/* Includes */
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <float.h>
+
+/* Prototypes */
+void read_file(char* file_name, int stats[]);
+
+#endif // _UTIL_H_
