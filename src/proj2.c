1e0864cfac7372d0c067a2af422822634a16d562
cyril-casapao <casapaoc@gmail.com>
2016-03-19 17:42:21 -0400

    Add work on simulation() and related functions.

--- src/proj2.c
+++ src/proj2.c
@@ -1,16 +1,30 @@
 /**
 * proj2.c
 *
+* This program simulates the work day at a bank and collects several statistics
+* about the bank's efficiency. These statistics are as follows:
+*
+*   1) Total number of customers served in one day
+*   2) Average time customers wait in line
+*   3) Max time a customer waits in line
+*   4) Average length of waiting line
+*   5) Max length of waiting line
+*
 * @author Cyril Casapao
 */
 
-#include <stdio.h>
 #include "../inc/stats.h"
+#include "../inc/queue.h"
 
 // Change FILE_NAME to  use a different data file
 #define FILE_NAME "../proj2.dat"
 
-void simulation(int numTellers);
+// The length of the work day to simulate (in minutes)
+#define DAY_LENGTH 480
+
+/* Function prototypes */
+void simulation(int num_tellers);
+int customer_arrival(int stats[]);
 
 /**
 * The main function. It simply asks the user to specify the number of tellers
@@ -21,11 +35,11 @@ void simulation(int numTellers);
 int main() {
     printf("Welcome to Bank Simulator!\n\n");
     printf("Please enter the number of tellers: ");
-    int numTellers;
-    scanf("%d", &numTellers);
+    int num_tellers;
+    scanf("%d", &num_tellers);
 
-    printf("Running simulation with %d tellers...\n\n", numTellers);
-    simulation(numTellers);
+    printf("Running simulation with %d tellers...\n\n", num_tellers);
+    simulation(num_tellers);
     return 0;
 }
 
@@ -33,12 +47,66 @@ int main() {
 /**
 * Runs the simulation.
 *
+* @TODO: Figure out how to represent tellers.
+*
 * @param numTellers
 *   The number of tellers
 */
-void simulation(int numTellers) {
+void simulation(int num_tellers) {
 
-    // Read the data file
+    /* Read the data file */
     int stats[5];
     read_file(FILE_NAME, stats);
+
+    /* Initialize statistics */
+    int total_customers = 0; // This doubles as customer ID
+    int sim_time;
+    double service_time;
+
+    /* Run the simulation for the work day */
+    for(sim_time = 0; sim_time < DAY_LENGTH; sim_time++) {
+
+        /* First, a random number of customers arrives */
+        int num_arrivals = customer_arrival(stats);
+        printf("%d customers have arrived!\n", num_arrivals);
+        int current_customer = 0;
+
+        /* Enqueue each new arrival */
+        while(current_customer < num_arrivals) {
+            enque(total_customers++);
+            current_customer++;
+        }
+
+        /* Deque customers */
+        while(!is_empty()) {
+            int removed = deque();
+            // printf("Customer %d is done!\n", removed);
+        }
+
+        /* Get the amount of time taken to service a customer
+        service_time = expdist(AVG_SERVICE);
+        printf("sim_time = %d, service_time = %0.5f\n", sim_time, service_time); */
+    }
+
+
+}
+
+
+/**
+* Determines how many customers arrive at any given minute.
+*
+* @param stats
+*   The array containing the customer arrival probabilities
+*/
+int customer_arrival(int stats[]) {
+    double random = rand() % 100;
+    int prev_range = 0;
+    int i;
+
+    for(i = 0; i < 5; i++) {
+        if(random < stats[i] + prev_range)
+            break;
+        prev_range += stats[i];
+    }
+    return i;
 }
